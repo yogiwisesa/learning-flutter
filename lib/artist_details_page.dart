36e4f3f89388dcf80389f9bffffe5f60cd920217
Yogi Wisesa <yogiwisesa@gmail.com>
2018-06-13 21:09:12 +0700

    Starterpack

---
+++ lib/artist_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:belajarflutter/models.dart';
+import 'package:flutter/material.dart';
+
+class ArtistDetailsPage extends StatelessWidget {
+  ArtistDetailsPage(this.artist);
+  final Artist artist;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
