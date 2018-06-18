e1f9d3f3b17ffa15252750a29a523ce3e358c612
yogiwisesa <yogiwisesa@gmail.com>
2018-06-18 13:06:26 +0700

    Finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,87 @@
-import 'package:belajarflutter/ui/artist_details_animator.dart';
-import 'package:belajarflutter/ui/artist_details_page.dart';
-import 'package:belajarflutter/data/models.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return MaterialApp(
+      home: new ChatItemScreen(),
+    );
+  }
+}
 
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+class ChatItemScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Chat Item"),
+        backgroundColor: Colors.lightGreen,
       ),
-      home: new ArtistsDetailsAnimator(),
+      body: new ChatItem(),
+    );
+  }
+}
+
+class ChatItem extends StatelessWidget {
+
+  final leftSection = new Container(
+    child: new CircleAvatar(
+      backgroundColor: Colors.lightGreen,
+      radius: 24.0,
+    ),
+  );
+
+  final middleSection = new Expanded(
+    child: new Container(
+      padding: new EdgeInsets.only(left: 8.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Text(
+            "Name",
+            style: new TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 16.0),
+          ),
+          new Text(
+            "Hi Whatsup?",
+            style: new TextStyle(color: Colors.grey),
+          )
+        ],
+      ),
+    ),
+  );
+  final rightSection = new Container(
+    child: new Column(
+      children: <Widget>[
+        new Text("9.50",
+        style: new TextStyle(
+          color: Colors.lightGreen,
+          fontSize: 12.0
+        ),),
+        new CircleAvatar(
+          backgroundColor: Colors.lightGreen,
+          radius: 10.0,
+          child: new Text("2",
+          style: new TextStyle(color: Colors.white, fontSize: 12.0),),
+        )
+      ],
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Row(
+            children: <Widget>[leftSection, middleSection, rightSection],
+          ),
+        ]
+      ),
     );
   }
 }
